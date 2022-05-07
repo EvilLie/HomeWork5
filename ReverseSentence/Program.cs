2d7551ecd866108f885b300aa16bfc70b73acc6a
Artemiy Sadriev <mosin1898@mail.ru>
2022-05-07 18:44:49 +0300

    Добавьте файлы проекта.

---
+++ ReverseSentence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWork5
+{
+    internal class Program
+    {
+        public static void Output(string text)
+        {
+            Console.WriteLine(text);
+        }
+        public static string[] Split(string userStr)
+        {
+            string[] splittedStr = userStr.Split(' ');
+            return splittedStr;
+        }
+        public static void PrintReverse(string[] splittedStr)
+        {
+            Array.Reverse(splittedStr);
+            foreach (string str in splittedStr)
+            {
+                
+                Output($"{str}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Output("Please enter your sentence: ");
+            string userStr = Console.ReadLine();
+            string[] splittedStr = Split(userStr);
+            PrintReverse(splittedStr);
+        }
+    }
+}
